fddfdf8878b1b29a8439e57b8a6e607fada9553f
Mubashwer Khurshid <mskh@student.unimelb.edu.au>
2016-04-29 19:59:19 +1000

    minor

--- src/aiproj/hexifence/mskgw/GWMSK.java
+++ src/aiproj/hexifence/mskgw/GWMSK.java
@@ -32,11 +32,11 @@ public class GWMSK implements Player, Piece {
         myColour = p;
         if(myColour == RED) {
             opColour = BLUE;
-            moveFinder = new MoveFinder3(game);
+            moveFinder = new MoveFinder(game);
         }
         else {
             opColour = RED;
-            moveFinder = new MoveFinder(game);
+            moveFinder = new MoveFinder2(game);
         }
         maxScore = game.getHexagons().size();
         
